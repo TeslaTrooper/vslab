67f249e9d0d22153f5301d559f94d15a78654308
Leonie Henner <hele1020@IWI-lkit3-03>
2019-08-01 13:49:42 +0200

    blub

--- user-service/user-service/src/main/java/de/hska/iwi/vslab/userservice/UserServiceApplication.java
+++ user-service/user-service/src/main/java/de/hska/iwi/vslab/userservice/UserServiceApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableResourceServer
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
